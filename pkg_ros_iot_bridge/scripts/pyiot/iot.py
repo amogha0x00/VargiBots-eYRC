98dfac609cb06d17495f18c62b95fe27289c894e
amogha0x00 <amoghavarshasg@gmail.com>
2020-10-19 11:55:54 +0530

    formating changes

--- pkg_ros_iot_bridge/scripts/pyiot/iot.py
+++ pkg_ros_iot_bridge/scripts/pyiot/iot.py
@@ -6,20 +6,19 @@
 
 """
 
-import paho.mqtt.client as mqtt
-import time
 from ast import literal_eval
+import paho.mqtt.client as mqtt
 import requests
 
 
 
 def on_connect(client, userdata, flags, rc):
-   print("Connected With Result Code "+str(rc))
+	print("Connected With Result Code "+str(rc))
 
 
 
-# This function subscribes to any mqtt topic given on "sub_topic" and makes "on_message" as on_message cb function
 def mqtt_subscribe_thread_start(on_message, broker_url, broker_port, sub_topic, qos):
+	# This function subscribes to any mqtt topic given on "sub_topic" and makes "on_message" as on_message cb function
 	client = mqtt.Client()
 	client.on_connect = on_connect
 	client.on_message = on_message
@@ -29,31 +28,31 @@ def mqtt_subscribe_thread_start(on_message, broker_url, broker_port, sub_topic, 
 	return rc
 
 
-# This function publish's on any mqtt topic given on "pub_topic" with message given as "payload"
 def mqtt_publish(broker_url, broker_port, pub_topic, payload, qos):
+	# This function publish's on any mqtt topic given on "pub_topic" with message given as "payload"
 	client = mqtt.Client()
 	client.connect(broker_url, broker_port)
 	client.loop_start()
 	pub_info = client.publish(pub_topic, payload, qos=qos, retain=False)
 	pub_info.wait_for_publish()
-	#time.sleep(2)
 	client.loop_stop()
 	return pub_info[0]
 
-'''
- This function makes a GET request to any webapp of given "spread_sheet_id" with any number of datapoints given through "data_points_dict_str" string
- ex: data_points_dict_str = "{ 'turtle_x': 10, 'turtle_y': 10, 'turtle_theta': 2}" pushes 10 to turtle_x and turtle_y column and 2 to
- turtle_theta column in this format there could be any number of data points
-'''
+
 def update_spreadsheet(spread_sheet_id, sheet_name, team_id, unique_id, data_points_dict_str):
+	'''
+		This function makes a GET request to any webapp of given "spread_sheet_id" with any number of datapoints 
+		given through "data_points_dict_str" string
+		ex: data_points_dict_str = "{ 'turtle_x': 10, 'turtle_y': 10, 'turtle_theta': 2}" pushes 10 to turtle_x and turtle_y column
+		and 2 to turtle_theta column in this format there could be any number of data points
+	'''
 	parameters = {'id':sheet_name, 'team_id':team_id, 'unique_id':unique_id}
 	url = "https://script.google.com/macros/s/" + spread_sheet_id + "/exec"
-	
+
 	data_points_dict = literal_eval(data_points_dict_str) # converts dict inside the string to dict
 	parameters.update(data_points_dict) # appends datapoints to parameters
-	
+
 	response = requests.get(url, params=parameters)
 	print('Made A GET request to :\n url: {}\n params: {}'.format(url, parameters))
 	print(response.content)
 	return not response.status_code == 200
-
